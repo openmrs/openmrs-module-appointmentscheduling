7439efa2ebabef8e1ce958465c3fea21c2c01610
Pamela Canchanya <pam.cdm@gmail.com>
2014-04-03 10:53:00 -0400

    UHM-1172 Fix method to get all the statuses apppointments

--- omod/src/main/java/org/openmrs/module/appointmentscheduling/rest/resource/openmrs1_9/AppointmentStatusResource1_9.java
+++ omod/src/main/java/org/openmrs/module/appointmentscheduling/rest/resource/openmrs1_9/AppointmentStatusResource1_9.java
@@ -7,11 +7,12 @@ import org.openmrs.module.webservices.rest.web.RequestContext;
 import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.annotation.Resource;
 import org.openmrs.module.webservices.rest.web.resource.api.Listable;
+import org.openmrs.module.webservices.rest.web.resource.api.Searchable;
 import org.openmrs.module.webservices.rest.web.response.ResponseException;
 
 @Resource(name = RestConstants.VERSION_1 + AppointmentRestController.APPOINTMENT_SCHEDULING_REST_NAMESPACE
         + "/appointmentstatus", supportedClass = AppointmentStatusResource1_9.class, supportedOpenmrsVersions = "1.9.*")
-public class AppointmentStatusResource1_9 implements Listable {
+public class AppointmentStatusResource1_9 implements Listable, Searchable {
 	
 	@Override
 	public SimpleObject getAll(RequestContext requestContext) throws ResponseException {
@@ -24,5 +25,10 @@ public class AppointmentStatusResource1_9 implements Listable {
 	public String getUri(Object o) {
 		return RestConstants.URI_PREFIX + "/appointmentscheduling/appointmentstatus/"
 		        + ((Appointment.AppointmentStatus) o).getName();
+	}
+	
+	@Override
+	public SimpleObject search(RequestContext context) throws ResponseException {
+		return null; //To change body of implemented methods use File | Settings | File Templates.
 	}
 }
