4d1a3ecfdd460df3665fa8b5dbbddd09d2f745c5
Kristopher Schmidt <kris.schmidt7@gmail.com>
2015-03-08 15:46:09 -0400

    AM-170: update resources to support 1.12.x

--- omod/src/main/java/org/openmrs/module/appointmentscheduling/rest/resource/openmrs1_9/AppointmentAllowingOverbookResource1_9.java
+++ omod/src/main/java/org/openmrs/module/appointmentscheduling/rest/resource/openmrs1_9/AppointmentAllowingOverbookResource1_9.java
@@ -6,7 +6,7 @@ import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.annotation.Resource;
 
 @Resource(name = RestConstants.VERSION_1 + AppointmentRestController.APPOINTMENT_SCHEDULING_REST_NAMESPACE
-        + "/appointmentallowingoverbook", supportedClass = Appointment.class, supportedOpenmrsVersions = {"1.9.*", "1.10.*", "1.11.*"})
+        + "/appointmentallowingoverbook", supportedClass = Appointment.class, supportedOpenmrsVersions = {"1.9.*", "1.10.*", "1.11.*", "1.12.*"})
 public class AppointmentAllowingOverbookResource1_9 extends AppointmentResource1_9 {
 	
 	@Override
