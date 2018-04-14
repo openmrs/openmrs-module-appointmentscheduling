3cc9210925ac9f9cca30797060995958c8bcbe74
Jacob Ryan Petersen <jacob.petersen@huskers.unl.edu>
2018-04-13 19:59:33 -0500

    Added @Authorized annotations to service methods
    AppointmentQueryService.java:
    Added authorized annotations to service methods to ensure you are logged
    in before accessing services. Privilege values may be added to the
    annotations as permissions are set up and configured.
    AppointmentDataService.java:
    Added authorized annotations to service methods to ensure you are logged
    it before accessing services. Privilege values may be added to the
    annotations as permissions are set up and configured.

--- api/src/main/java/org/openmrs/module/appointmentscheduling/reporting/query/service/AppointmentQueryService.java
+++ api/src/main/java/org/openmrs/module/appointmentscheduling/reporting/query/service/AppointmentQueryService.java
@@ -1,6 +1,7 @@
 package org.openmrs.module.appointmentscheduling.reporting.query.service;
 
 import org.openmrs.module.appointmentscheduling.reporting.query.definition.AppointmentQuery;
+import org.openmrs.annotation.Authorized;
 import org.openmrs.module.appointmentscheduling.reporting.query.AppointmentQueryResult;
 import org.openmrs.module.reporting.definition.service.DefinitionService;
 import org.openmrs.module.reporting.evaluation.EvaluationContext;
@@ -12,9 +13,11 @@ import org.springframework.transaction.annotation.Transactional;
 public interface AppointmentQueryService extends DefinitionService<AppointmentQuery> {
 
     @Transactional(readOnly = true)
+    @Authorized()
     public AppointmentQueryResult evaluate(AppointmentQuery query, EvaluationContext context) throws EvaluationException;
 
     @Transactional(readOnly = true)
+    @Authorized()
     public AppointmentQueryResult evaluate(Mapped<? extends AppointmentQuery> mappedQuery, EvaluationContext context) throws EvaluationException;
 
 }
