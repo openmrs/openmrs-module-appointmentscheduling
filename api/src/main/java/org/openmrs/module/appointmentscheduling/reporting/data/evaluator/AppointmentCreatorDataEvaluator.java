515d05be076d8e72ceabcd06a5e0a34226c90b9a
Brandon Istenes <bistenes@gmail.com>
2021-07-01 14:55:33 -0700

    Add creator data defintion

---
+++ api/src/main/java/org/openmrs/module/appointmentscheduling/reporting/data/evaluator/AppointmentCreatorDataEvaluator.java
@@ -0,0 +1,14 @@
+package org.openmrs.module.appointmentscheduling.reporting.data.evaluator;
+
+import org.openmrs.annotation.Handler;
+import org.openmrs.module.appointmentscheduling.reporting.data.definition.AppointmentCreatorDataDefinition;
+
+@Handler(supports = AppointmentCreatorDataDefinition.class)
+public class AppointmentCreatorDataEvaluator extends AppointmentPropertyDataEvaluator {
+
+    @Override
+    public String getPropertyName() {
+        return "timeSlot.appointmentBlock.creator";
+    }
+
+}
